32bee30b4afa6a3a17aa5aac9f09923f8d15ffd4
Martin Terhes <martin.terhes@icloud.com>
2024-07-04 20:42:32 +0200

    delete: old model files

--- models2/Accident.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type Accident struct {
-	LicensePlate string `json:"licensePlate"`
-	AccidentDate string `json:"accidentDate"`
-	Role         string `json:"role"`
-}
--- models2/LicensePlate.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type LicensePlate struct {
-	LicensePlate string `json:"licensePlate" gorm:"primaryKey"`
-	Comment      string `json:"comment"`
-	CreatedAt    string `json:"createdAt"`
-	UpdatedAt    string `json:"updatedAt"`
-}
