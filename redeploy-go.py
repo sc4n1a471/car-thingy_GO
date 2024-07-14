79d7f6702d669a4defd447fc64109c10512907a8
Martin Terhes <martin.terhes@icloud.com>
2024-07-14 22:07:39 +0200

    feat: added new redeploy script + updated workflows

---
+++ redeploy-go.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+import sys
+import json
+import docker
+
+def parse_payload():
+        payload = sys.argv[1]
+        payload_parsed = json.loads(payload)
+        version = payload_parsed['version']
+        env = payload_parsed['env']
+        print(f'Getting {version}...')
+        return version, env
+
+
+def main():
+        version, env = parse_payload()
+
+        if env == 'prod':
+                print("Redeploying production container...")
+                name = 'car-thingy_go_prod'
+                volumes = {'downloaded_images_prod': {'bind': '/app/downloaded_images', 'mode': 'rw'}}
+                environment = ["DB_USERNAME=<DB_USERNAME>", "DB_PASSWORD=<DB_PASSWORD>", "DB_IP=<DB_IP>", "DB_PORT=<DB_PORT>", "DB_NAME=<DB_NAME>"]
+                ports = {'3000/tcp': '<PORT_AS_INT>'}
+        else:
+                print("Redeploying development container...")
+                name = 'car-thingy_go_dev'
+                volumes = {'downloaded_images_dev': {'bind': '/app/downloaded_images', 'mode': 'rw'}}
+                environment = ["DB_USERNAME=<DB_USERNAME>", "DB_PASSWORD=<DB_PASSWORD>", "DB_IP=<DB_IP>", "DB_PORT=<DB_PORT>", "DB_NAME=<DB_NAME>"]
+                ports = {'3000/tcp': '<PORT_AS_INT>'}
+
+        client = docker.from_env()
+        try:
+                container = client.containers.get(name)
+                container.stop()
+                print("Stopped current version")
+        except:
+                pass
+        client.containers.prune()
+        print("Removed current version")
+
+        client.containers.run(
+                f"sc4n1a471/car-thingy_go:{version}",
+                detach = True,
+                volumes = volumes,
+                environment = environment,
+                ports = ports,
+                name = name,
+                restart_policy = {'Name': 'on-failure', 'MaximumRetryCount': 5}
+        )
+        print(f"Version {version} was deployed successfully")
+
+if __name__ == "__main__":
+        main()
