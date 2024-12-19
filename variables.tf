caba403cdc8b25dc71332995f29db2832f03fc18
Martin Terhes <martin.terhes@icloud.com>
2024-12-19 14:47:40 +0100

    feat: update version to 2.3 and added v1 of Jenkins/Terraform

---
+++ variables.tf
@@ -0,0 +1,51 @@
+variable "container_name" {
+  description = "Name of the container"
+  type = string
+}
+variable "container_version" {
+  description = "Version of the Docker image"
+  type        = string
+  default     = "latest-dev"
+}
+
+variable "env" {
+  description = "Environment to deploy (prod or dev)"
+  type        = string
+  default     = "dev"
+}
+
+variable "db_username" {
+  description = "Database username"
+  type        = string
+  sensitive   = true
+}
+
+variable "db_password" {
+  description = "Database password"
+  type        = string
+  sensitive   = true
+}
+
+variable "db_ip" {
+  description = "Database IP address"
+  type        = string
+  sensitive   = true
+}
+
+variable "db_port" {
+  description = "Database port"
+  type        = string
+  sensitive   = true
+}
+
+variable "db_name" {
+  description = "Database name"
+  type        = string
+  sensitive   = true
+}
+
+variable "api_secret" {
+  description = "API secret"
+  type        = string
+  sensitive   = true
+}
